0b84c596db808f94e62a0d9d2923098dadea97ad
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2024-09-21 15:27:22 +0530

    commit loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid } from '@/components/grid';
 import { Hero } from '@/components/hero';
 import Approch from '@/components/ui/approch';
@@ -10,7 +10,26 @@ import Footer from '@/components/ui/footer';
 import Recentproject from '@/components/ui/recentproject';
 import { navItems } from '@/data';
 
+const Loading = () => (
+  <div className="loader invert flex justify-center items-center h-screen ">
+   <div data-glitch="" className="glitch"><img src="/aviraj-logo.png" alt="" /></div>
+</div>
+);
+
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => setLoading(false), 2000); // Simulate a 2-second loading time
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
